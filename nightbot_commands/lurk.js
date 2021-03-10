ac368acd54e6d7c07b8ff6f061b5004343f1d4ce
DistractionCrab <convoluted.nonsense@gmail.com>
2021-03-10 01:49:00 -0500

    Updated some Nightbot Scripts

--- nightbot_commands/lurk.js
+++ nightbot_commands/lurk.js
@@ -1,24 +1,6 @@
-$(eval const responses = [
-	'a leaf',
-	'a statue with judging eyes',
-	'a living failure',
-	'an anime body pillow',
-	'a box',
-	'a stack of pizza boxes',
-	'pudding',
-	'a rubber ducky',
-	'William Shatner',
-	'a Messenger',
-	'a tree',
-	'a cuckoo clock',
-	'a waffle',
-	'a freshly seasoned ham',
-	'a plastic bag floating in the wind',
-	'a statue of a cat',
-	'a soda can',
-	'a SNES controller',
-	'Mario',
-	
-];
+$(eval 
 
-`$(user) used chameleon to transform into ${responses[Math.floor(Math.random() * responses.length)]}`)
+var values = "$(urlfetch https://pastebin.com/raw/97V99DAf) ".split(",");
+var choose = Math.floor(Math.random() * values.length);
+
+`$(user) used chameleon to transform into ${values[choose]}`)
